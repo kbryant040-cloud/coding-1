4f878c48989f89cce715d3535f4237d6f6645b5a
kbryant040-cloud <kbryant040@boyslatin.net>
2026-09-03 13:44:28 +0000

    classwork

---
+++ VariableActivity.py
@@ -0,0 +1,33 @@
+Variable - a container that stores data. You can 
+name your container almost anything you want, so long
+as it is descriptive.
+
+Name= "Kobe Bryant"
+yearsoft= 10
+graduate= True
+
+When we want to craete a variable, we make up a word,
+use the equal sign and then assign it a data type.
+whenever the computer sees our variable word it will
+always use the data inside.
+
+A variable Must start with letter and cannot start with a number
+A variable can be a mixd of letters and numbers But not symbols
+A variable Cannot be a python keyword
+
+
+Numberofapplesinasupermarket= 15
+Numberofstudentinamathclass= 12
+Travelinsuranceforyourtrip=add 
+Abronze,silver,andgoldmembershipforagym=25
+Priceof3differentdrinksataresturant=spritemountdewfanta
+
+Camel Case - when craeting a variable names with multiple
+
+
+Snake Case - when creating a variable using snake case 
+we use a undercore symbol to space out each new word
+the reason why its called snake case it bc the undercore
+represents a snake on the ground new word_
+username for new instagram accounts= 0
+password recovery tiktok account= 0
