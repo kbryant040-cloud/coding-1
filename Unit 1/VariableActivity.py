cd46969bb7829096c1bba8d373b794a02a4c2138
kbryant040-cloud <kbryant040@boyslatin.net>
2026-09-09 13:44:46 +0000

    Notes

--- Unit 1/VariableActivity.py
+++ Unit 1/VariableActivity.py
@@ -1,19 +1,19 @@
-Variable - a container that stores data. You can 
-name your container almost anything you want, so long
-as it is descriptive.
+# Variable - a container that stores data. You can 
+# name your container almost anything you want, so long
+# as it is descriptive.
 
 Name= "Kobe Bryant"
 yearsoft= 10
 graduate= True
 
-When we want to craete a variable, we make up a word,
-use the equal sign and then assign it a data type.
-whenever the computer sees our variable word it will
-always use the data inside.
+# When we want to craete a variable, we make up a word,
+# use the equal sign and then assign it a data type.
+# whenever the computer sees our variable word it will
+# always use the data inside.
 
-A variable Must start with letter and cannot start with a number
-A variable can be a mixd of letters and numbers But not symbols
-A variable Cannot be a python keyword
+# A variable Must start with letter and cannot start with a number
+# A variable can be a mixd of letters and numbers But not symbols
+# A variable Cannot be a python keyword
 
 
 Numberofapplesinasupermarket= 15
@@ -22,12 +22,12 @@ Travelinsuranceforyourtrip=add
 Abronze,silver,andgoldmembershipforagym=25
 Priceof3differentdrinksataresturant=spritemountdewfanta
 
-Camel Case - when craeting a variable names with multiple
+# Camel Case - when craeting a variable names with multiple
 
 
-Snake Case - when creating a variable using snake case 
-we use a undercore symbol to space out each new word
-the reason why its called snake case it bc the undercore
-represents a snake on the ground new word_
-username for new instagram accounts= 0
-password recovery tiktok account= 0
+# Snake Case - when creating a variable using snake case 
+# we use a undercore symbol to space out each new word
+# the reason why its called snake case it bc the undercore
+# represents a snake on the ground new word_
+# username for new instagram accounts= 0
+# password recovery tiktok account= 0
---
+++ Unit 1/operators.py
@@ -0,0 +1,30 @@
+# Assignment Operators- Exclusively used to 
+# assign values to variables
+# key / value parings
+
+# We use a single equal sign to respresent the assignment
+# operator
+name = "bryant"
+grade = 10
+school = True
+
+# Arithmetic Operators - Used on numerical
+# data types to perform calculations.
+# intergers (whole numbers) and floats (decimal numbers)
+
+# print is a function that lets us show code
+# in the terminal
+print(3 + 3) # addition operator
+print(3 - 3) # subtraction operator
+print(12 / 3) # division operator
+print(3 * 4) # multiplication operator
+
+# Comparison Operators - Set of symbols used
+# to assess if data is the same or different and
+# how they differ
+
+print(10 > 1) # greater than operator
+print(10 < 2) # less than operator
+
+# 2 equal signs compare if something is the same
+print ("kobe" == "Kobe") # same as
